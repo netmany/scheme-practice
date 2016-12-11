b658f9b383c71e52231329ef131fcb83c44f14b0
netone <netreader@163.com>
2016-12-11 18:18:03 +0800

    Create native.c

---
+++ ffi/native.c
@@ -0,0 +1,28 @@
+// file ffi.c
+
+#include <string.h>
+
+// test ffi arguments passing
+
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+
+int passVal(int srcInt, int* dstInt,
+            const char* srcStr, char* dstStr, size_t dstStrLen) {
+    *dstInt = srcInt + 1;
+    strncpy(dstStr, srcStr, max(dstStrLen, sizeof(srcStr)));
+    return 0;
+}
+
+struct info {
+	char* name;
+	int age;
+};
+
+int getInfo(const char* name, struct info* p){
+     if(strcmp(name, "helen") == 0) {
+     	p->age = 20;
+     } else {
+     	p->age = 0;
+     }
+     return 0;
+}
